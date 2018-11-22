991a31c2079005383b86c52ab81e8e4bc3fb4699
Eshan-k <eshan.kulkarni15@gmail.com>
2018-11-22 16:25:03 -0500

    fix: edited faculty pages

--- application/views/detail_professor_ashton.php
+++ application/views/detail_professor_ashton.php
@@ -5,92 +5,123 @@
 		<div class="block-header">
 			<h2></h2>
 		</div>
-		
-		<!-- CPU Usage -->
-		<div class="row clearfix">
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<div class="card">
-					<div class="header">
-						<h3 class="text-black">Professor Ashton</h3>
-						<h2>CS - 645, Internet and Higher Level Protocol
-							<span><a href="#">LinkedIn</a></span></h2>
-						<span class="header-dropdown m-r--5">
+
+        <!-- Faculty info Start-->
+        <div class="row clearfix">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="card">
+                    <div class="header">
+                        <h3 class="text-black">Professor Ashton</h3>
+                        <h2>CS - 645, Internet and Higher Level Protocol
+                            <span><a href="#">LinkedIn</a></span></h2>
+                        <span class="header-dropdown m-r--5">
 							<h3>Overall Rating: <span class="label bg-red">2</span></h3>
 						</span>
-					</div>
-					
-					<div class="body">
-						<h2 class="card-inside-title">
+                    </div>
+
+                    <div class="body">
+                        <h2 class="card-inside-title">
                             <img src="../../public/images/ash.jpeg" style="width: 250px" height="250px" class="img-responsive">
                         </h2>
-						<div class="row clearfix">
-							<div class="col-sm-12">
-								<div class="form-group">
-									<h4>
-										My name is Ashton,
-										I teaching this subject from the past 5 years.
-										<br>
-									</h4>
-								</div>
-							</div>
-						</div>
-						<label class="menu-toggle waves-effect waves-block disabled">
-							<i class="material-icons">add</i> &nbsp; &nbsp;
-							<span class="h4">
-								Area of Research
-							</span>
-						
-						</label>
-						
-						<ul class="ml-menu" style="display: none;">
-							<h4></h4>
-						</ul>
-						<div><br></div>
-						<div class="body">
-							<div class="table-responsive">
-								<h4>Reviews & Ratings</h4>
-								<div id="DataTables_Table_0_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
-									<div class="row">
-										<div class="col-lg-12">
-											<table class="table table-hover js-basic-example dataTable" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
-												<thead>
-												<tr role="row">
-													<th class="sorting_asc" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1"  aria-sort="ascending" colspan="1">Date</th>
-													<th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Name</th>
-													<th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Review</th>
-													<th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Rating</th>
-												</tr>
-												</thead>
-												<tbody>
-												<tr role="row" class="odd">
-													<td>1/1/2016</td>
-													<td>Student 1q</td>
-													<td>Lots of Assignment.</td>
-													<td>2</td>
-												</tr>
-												<tr role="row" class="even">
-													<td>2/2/2016</td>
-													<td>Anonymous</td>
-													<td>Lot of assignment, & does'nt teach anything, Will not take his class again</td>
-													<td>2</td>
-												</tr>
-												<tr role="row" class="odd">
-													<td>3/3/2017</td>
-													<td>Student 3c</td>
-													<td>Learned nothing.</td>
-													<td>1.5</td>
-												</tr>
-												</tbody>
-											</table>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+                        <div class="row clearfix">
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <h3>
+                                        My name is Ashton,
+                                        I teaching this subject from the past 5 years.
+                                        <br><br>
+                                    </h3>
+                                    <h4>
+                                        Office Hours: Tuesday, 1pm - 4pm<br><br>
+                                        Email: ashton@faculty.com
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Faculty info end-->
+
+        <!-- Area of Research Start-->
+        <div class="row clearfix">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="card">
+                    <div class="header">
+                        <label class="menu-toggle waves-effect waves-block">
+                            <span class="header-dropdown m-r--5"><i class="material-icons">add</i></span>
+                            <h3 class="text-black">Area of Research</h3>
+                        </label>
+                        <ul class="ml-menu" style="display: none;">
+                            <h4></h4>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Area of Research End-->
+
+        <!-- Ratings and Reviews Start-->
+        <div class="row clearfix">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="card">
+                    <div class="header">
+                        <h3 class="text-black">Ratings & Reviews</h3>
+                    </div>
+
+                    <div class="body">
+                        <div class="table-responsive">
+                            <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <table class="table table-hover js-basic-example dataTable" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
+                                            <thead>
+                                            <tr role="row">
+                                                <th class="sorting_desc" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1"  aria-sort="descending" colspan="1">Date</th>
+                                                <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Name</th>
+                                                <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Review</th>
+                                                <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Rating</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr role="row" class="odd">
+                                                <td>1/1/2016</td>
+                                                <td>Student 1q</td>
+                                                <td>Tough Grader.</td>
+                                                <td>2</td>
+                                            </tr>
+                                            <tr role="row" class="even">
+                                                <td>2/2/2016</td>
+                                                <td>Anonymous</td>
+                                                <td>Lot of assignment, & does'nt teach anything, Will not take his class again</td>
+                                                <td>2</td>
+                                            </tr>
+                                            <tr role="row" class="odd">
+                                                <td>3/3/2017</td>
+                                                <td>Student 3c</td>
+                                                <td>Learned nothing.</td>
+                                                <td>1.5</td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-7">
+                                        <div class="dataTables_paginate paging_simple_numbers" id="DataTables_Table_0_paginate">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Ratings and Reviews End-->
 	</div>
 </section>
 
--- application/views/detail_professor_samantha.php
+++ application/views/detail_professor_samantha.php
@@ -5,122 +5,148 @@
 		<div class="block-header">
 			<h2></h2>
 		</div>
-  
-		<div class="row clearfix">
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<div class="card">
-					<div class="header">
-						<h3 class="text-black">Professor Johnson</h3>
-						<h2>CS - 634, Data Mining&nbsp;
-							<span><a target="_blank" href="#">LinkedIn</a></span></h2>
-						<span class="header-dropdown m-r--5">
+
+        <!-- Faculty info Start-->
+        <div class="row clearfix">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="card">
+                    <div class="header">
+                        <h3 class="text-black">Professor Johnson</h3>
+                        <h2>CS - 634, Data Mining&nbsp;
+                            <span><a target="_blank" href="#">LinkedIn</a></span></h2>
+                        <span class="header-dropdown m-r--5">
 							<h3>Overall Rating: <span class="label bg-green">4</span></h3>
 						</span>
-					</div>
-					
-					<div class="body">
-						<h2 class="card-inside-title">
+                    </div>
+
+                    <div class="body">
+                        <h2 class="card-inside-title">
                             <img src="../../public/images/643.jpeg" style="width: 250px" height="250px" class="img-responsive">
                         </h2>
-						<div class="row clearfix">
-							<div class="col-sm-12">
-								<div class="form-group">
-									<h4>
-										My name is Samantha Johnson, I have done my master's in computer science, from
-										University of Texas - Austin.
-										I teaching this subject from the past 16 years.
-										<br>
-									</h4>
-								</div>
-							</div>
-						</div>
-						<label class="menu-toggle waves-effect waves-block">
-							<i class="material-icons">add</i> &nbsp; &nbsp;
-							<span class="h4">
-								Area of Research
-							</span>
-							
-						</label>
-						
-						<ul class="ml-menu" style="display: none;">
-							<h4>
-								Interested in Deep Learning. Doing research on <i>xyz</i>.
-								<br>
-								In deep learning, each level learns to transform its input data into a slightly
-								more abstract and composite representation. In an image recognition application,
-								the raw input may be a matrix of pixels; the first representational layer may
-								abstract the pixels and encode edges; the second layer may compose and encode
-								arrangements of edges; the third layer may encode a nose and eyes; and the
-								fourth layer may recognize that the image contains a face. Importantly, a deep
-								learning process can learn which features to optimally place in which level on
-								its own.
-							</h4>
-						</ul>
-						<div><br></div>
-						<div class="body">
-							<div class="table-responsive">
-								<h4>Reviews & Ratings</h4>
-								<div id="DataTables_Table_0_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
-									<div class="row">
-										<div class="col-lg-12">
-											<table class="table table-hover js-basic-example dataTable" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
-												<thead>
-												<tr role="row">
-													<th class="sorting_desc" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1"  aria-sort="descending" colspan="1">Date</th>
-													<th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Name</th>
-													<th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Review</th>
-													<th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Rating</th>
-												</tr>
-												</thead>
-												<tbody>
-												<tr role="row" class="odd">
-													<td>7/26/2017</td>
-													<td>Student 1a</td>
-													<td>Great Professor, learned a lot.</td>
-													<td>4</td>
-												</tr>
-												<tr role="row" class="even">
-													<td>2/11/2017</td>
-													<td>Student 2b</td>
-													<td>Lot of assignment.</td>
-													<td>3.5</td>
-												</tr>
-												<tr role="row" class="odd">
-													<td>12/22/2016</td>
-													<td>Student 3c</td>
-													<td>Great learning experience.</td>
-													<td>4</td>
-												</tr>
-												<tr role="row" class="even">
-													<td>11/2/2016</td>
-													<td>Student 4d</td>
-													<td>Nice Professor, easy grader.</td>
-													<td>4</td>
-												</tr>
-												<tr role="row" class="odd">
-													<td>10/1/2016</td>
-													<td>Student 5e</td>
-													<td>Professor used to work for acm corp evident from the teaching. Great Professor.</td>
-													<td>4</td>
-												</tr>
-												</tbody>
-											</table>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-sm-7">
-											<div class="dataTables_paginate paging_simple_numbers" id="DataTables_Table_0_paginate">
-											
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+                        <div class="row clearfix">
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <h3>
+                                        My name is Samantha Johnson, I have done my master's in computer science, from
+                                        University of Texas - Austin.
+                                        I teaching this subject from the past 16 years.
+                                        <br><br>
+                                    </h3>
+                                    <h4>
+                                        Office Hours: Wednesday, 1pm - 4pm<br><br>
+                                        Email: johnson@faculty.com
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Faculty info end-->
+
+        <!-- Area of Research Start-->
+        <div class="row clearfix">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="card">
+                    <div class="header">
+                        <label class="menu-toggle waves-effect waves-block">
+                            <span class="header-dropdown m-r--5"><i class="material-icons">add</i></span>
+                            <h3 class="">Area of Research</h3>
+                        </label>
+                        <ul class="ml-menu" style="display: none;">
+                            <h4>
+                                Interested in Deep Learning. Doing research on <i>xyz</i>.
+                                <br>
+                                In deep learning, each level learns to transform its input data into a slightly
+                                more abstract and composite representation. In an image recognition application,
+                                the raw input may be a matrix of pixels; the first representational layer may
+                                abstract the pixels and encode edges; the second layer may compose and encode
+                                arrangements of edges; the third layer may encode a nose and eyes; and the
+                                fourth layer may recognize that the image contains a face. Importantly, a deep
+                                learning process can learn which features to optimally place in which level on
+                                its own.
+                            </h4>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Area of Research End-->
+
+        <!-- Ratings and Reviews Start-->
+        <div class="row clearfix">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="card">
+                    <div class="header">
+                        <h3 class="text-black">Ratings & Reviews</h3>
+                    </div>
+
+                    <div class="body">
+                        <div class="table-responsive">
+                            <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <table class="table table-hover js-basic-example dataTable" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
+                                            <thead>
+                                            <tr role="row">
+                                                <th class="sorting_desc" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1"  aria-sort="descending" colspan="1">Date</th>
+                                                <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Name</th>
+                                                <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Review</th>
+                                                <th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1">Rating</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr role="row" class="odd">
+                                                <td>7/26/2017</td>
+                                                <td>Student 1a</td>
+                                                <td>Nice Faculty, learned a lot.</td>
+                                                <td>4</td>
+                                            </tr>
+                                            <tr role="row" class="even">
+                                                <td>2/11/2017</td>
+                                                <td>Student 2b</td>
+                                                <td>Lot of assignment.</td>
+                                                <td>3.5</td>
+                                            </tr>
+                                            <tr role="row" class="odd">
+                                                <td>12/22/2016</td>
+                                                <td>Student 3c</td>
+                                                <td>Good work experience.</td>
+                                                <td>4</td>
+                                            </tr>
+                                            <tr role="row" class="even">
+                                                <td>11/2/2016</td>
+                                                <td>Student 4d</td>
+                                                <td>Teaches through industry experience.</td>
+                                                <td>4</td>
+                                            </tr>
+                                            <tr role="row" class="odd">
+                                                <td>10/1/2016</td>
+                                                <td>Student 5e</td>
+                                                <td>Nice Faculty.</td>
+                                                <td>4</td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-7">
+                                        <div class="dataTables_paginate paging_simple_numbers" id="DataTables_Table_0_paginate">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Ratings and Reviews End-->
+        
 	</div>
 </section>
 
